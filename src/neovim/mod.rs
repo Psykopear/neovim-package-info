37832e214f8585350da99a8d83db0dc45168c3d3
Federico Dolce <psykopear@gmail.com>
2019-04-27 20:22:33 +0200

    Added loading state and parallelized calls

--- src/neovim/mod.rs
+++ src/neovim/mod.rs
@@ -1,6 +1,7 @@
 use crate::fetcher::{Cratesio, Npm, Pypi, Store};
 use cargo_toml::{Dependency, Manifest};
 use neovim_lib::{Neovim, NeovimApi, Session, Value};
+use rayon::prelude::*;
 use semver::{Version, VersionReq};
 use std::fs;
 
@@ -51,9 +52,10 @@ impl EventHandler {
         }
     }
 
-    fn set_text(&mut self, message: &str, line_number: i64) {
+    fn set_text(&mut self, message: &str, line_number: i64, highlight: &str) {
         if let Ok(buffer) = self.nvim.get_current_buf() {
-            let chunks: Vec<Value> = vec![vec![Value::from(message)].into()];
+            let chunks: Vec<Value> =
+                vec![vec![Value::from(message), Value::from(highlight)].into()];
             match buffer.set_virtual_text(&mut self.nvim, 0, line_number, chunks, vec![]) {
                 Ok(_) => (),
                 Err(error) => self.echo(&format!("{}", error)),
@@ -71,27 +73,41 @@ impl EventHandler {
             .unwrap();
     }
 
-    fn check_dependency<T: Store>(&self, name: &str, dependency: &Dependency, store: &T) -> String {
+    fn check_dependency<T: Store>(
+        &self,
+        name: &str,
+        dependency: &Dependency,
+        store: &T,
+    ) -> (String, String) {
         if let Ok(requirement) = Version::parse(dependency.req()) {
             if let Ok(store_version) = store.get_max_version(name) {
                 if let Ok(latest_version) = Version::parse(&store_version) {
                     if latest_version.major > requirement.major {
-                        format!("  RED {}", latest_version)
+                        (format!("  => {}", latest_version), "Error".to_string())
                     } else if latest_version.minor > requirement.minor {
-                        format!("  YELLOW {}", latest_version)
+                        (format!("  => {}", latest_version), "Number".to_string())
                     } else if latest_version.patch > requirement.patch {
-                        format!("  GREEN {}", latest_version)
+                        (format!("  => {}", latest_version), "String".to_string())
                     } else {
-                        format!("  {}", latest_version)
+                        (format!("  => {}", latest_version), "Comment".to_string())
                     }
                 } else {
-                    format!("Error parsing store version {}", store_version)
+                    (
+                        format!("  => Error parsing store version {}", store_version),
+                        "Comment".to_string(),
+                    )
                 }
             } else {
-                format!("Error getting store version for {}", name)
+                (
+                    format!("  => Error getting store version for {}", name),
+                    "Comment".to_string(),
+                )
             }
         } else {
-            format!("Error parsing {}", name)
+            (
+                format!("  => Error parsing {}", name),
+                "Comment".to_string(),
+            )
         }
     }
 
@@ -104,24 +120,42 @@ impl EventHandler {
                     let file_path = &parse_string(&args[0]).expect("File path not received!");
                     let content = fs::read_to_string(file_path).expect("Can't read to string");
                     let cargo_toml = Manifest::from_str(&content).expect("Can't parse cargo toml");
-                    for (name, dependency) in cargo_toml.dependencies {
+
+                    let dependencies = cargo_toml
+                        .dependencies
+                        .iter()
+                        .chain(cargo_toml.dev_dependencies.iter())
+                        .chain(cargo_toml.build_dependencies.iter());
+                    for dep in dependencies {
+                        let mut line_number = 0;
+                        for (index, line) in content.split("\n").enumerate() {
+                            if line.to_string().starts_with(&format!("{} = ", dep.0)) {
+                                line_number = index
+                            }
+                        }
+                        self.set_text("  => Loading...", line_number as i64, "Comment");
+                    }
+
+                    let dependencies: Vec<(String, (String, String))> = cargo_toml
+                        .dependencies
+                        .par_iter()
+                        .chain(cargo_toml.dev_dependencies.par_iter())
+                        .chain(cargo_toml.build_dependencies.par_iter())
+                        .map(|(name, dependency)| {
+                            (
+                                name.to_string(),
+                                self.check_dependency(&name, &dependency, &self.cratesio),
+                            )
+                        })
+                        .collect();
+                    for (name, (text, highlight_group)) in dependencies {
                         let mut line_number = 0;
                         for (index, line) in content.split("\n").enumerate() {
                             if line.to_string().starts_with(&format!("{} = ", name)) {
                                 line_number = index
                             }
                         }
-                        let res = self.check_dependency(&name, &dependency, &self.cratesio);
-                        self.echo(&res);
-                        self.set_text(&res, line_number as i64);
-                    }
-                    for (name, dependency) in cargo_toml.dev_dependencies {
-                        let res = self.check_dependency(&name, &dependency, &self.cratesio);
-                        self.echo(&res);
-                    }
-                    for (name, dependency) in cargo_toml.build_dependencies {
-                        let res = self.check_dependency(&name, &dependency, &self.cratesio);
-                        self.echo(&res);
+                        self.set_text(&text, line_number as i64, &highlight_group);
                     }
                 }
                 Messages::Pipfile => {
