119d66819879a5c987060de33c5a85324362b19a
Federico Dolce <psykopear@gmail.com>
2019-05-13 19:43:57 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Download binary built by travis inside plugin directory
+wget https://github.com/Psykopear/neovim-package-info/releases/download/0.1.0/neovim-package-info -O plugin/neovim-package-info
+chmod +x plugin/neovim-package-info
 
 # Remove things we don't want
 rm -rf target images examples Cargo.lock Cargo.toml README.md src
